d21ed110f34a3a303eca1f7e229688d7c5f7b9e9
Asilbek0709 <tasilbek348@gmail.com>
2025-12-31 13:20:10 +0500

    Fixes

--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -7,9 +7,14 @@ import Link from "next/link";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { useTranslation } from "react-i18next";
 
 
 export default function Slider() {
+
+
+    const { t } = useTranslation();
+
     return(
         <Swiper 
             modules={[Navigation, Pagination, Autoplay]}
@@ -36,7 +41,7 @@ export default function Slider() {
                 <div className="slide" style={{ backgroundImage: "url('/solar2.jpg')"}}>
                     <div className="overlay" >
                         <h1>XORAZM SARIMAY 126MW PROJECT</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide>
@@ -44,7 +49,7 @@ export default function Slider() {
                 <div className="slide" style={{ backgroundImage: "url('/solar3.jpg')"}}>
                     <div className="overlay">
                         <h1>XORAZM SARIMAY 126MW PROJECT</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide>
@@ -52,7 +57,7 @@ export default function Slider() {
                 <div className="slide" style={{ backgroundImage: "url('/solar4.jpg')"}}>
                     <div className="overlay">
                         <h1>YANGI YO'L PLANT</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide>
@@ -60,7 +65,7 @@ export default function Slider() {
                 <div className="slide" style={{ backgroundImage: "url('/solar5.jpg')"}}>
                     <div className="overlay">
                         <h1>BEKABAD MANUFACTURING PLANT</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide>
@@ -68,15 +73,15 @@ export default function Slider() {
                 <div className="slide" style={{ backgroundImage: "url('/solar7.jpg')"}}>
                     <div className="overlay">
                         <h1>CONSTRUCTION OF A 30 KW SOLAR PHOTOVOLTAIC POWER PLANT AT KORLI-TOG STATION</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div className="slide" style={{ backgroundImage: "url('/solar8.jpg')"}}>
                     <div className="overlay">
-                        <h1>Waste Water Treatment Park</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <h1>200 kW solar photovoltaic power plant</h1>
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide>
@@ -84,7 +89,7 @@ export default function Slider() {
                 <div className="slide" style={{ backgroundImage: "url('/solar1.jpg')"}}>
                     <div className="overlay">
                         <h1>XORAZM SARIMAY 126MW PROJECT</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide> 
@@ -92,15 +97,15 @@ export default function Slider() {
                 <div className="slide" style={{ backgroundImage: "url('/solar9.jpg')"}}>
                     <div className="overlay">
                         <h1>PRESIDENTIAL SCHOOL</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div className="slide" style={{ backgroundImage: "url('/solar10.jpg')"}}>
                     <div className="overlay">
-                        <h1>Waste Water Treatment Park</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <h1>PRESIDENTIAL SCHOOL</h1>
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide>
@@ -108,7 +113,7 @@ export default function Slider() {
                 <div className="slide" style={{ backgroundImage: "url('/solar11.jpg')"}}>
                     <div className="overlay">
                         <h1>SOLAR PHOTOVOLTAIC POWER PLANT</h1>
-                        <Link className='hover-link' href={'/projects'}>Projects</Link>             
+                        <Link className='hover-link' href={'/projects'}>{t("dropdown-link-project")}</Link>             
                     </div>
                 </div>
             </SwiperSlide>
